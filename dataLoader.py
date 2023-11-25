1815720ff7998a2bd0aa60a766382859a9c2831b
asiter <1139933860@qq.com>
2023-11-25 12:08:35 +0800

    review:Rewrite the data loader tool

--- dataLoader.py
+++ dataLoader.py
@@ -1,6 +1,14 @@
+import random
+
+import numpy
+import soundfile
+import torch
+
+
 class train_loader(object):
-    def __init__(self, train_list, train_path, **kwargs):
+    def __init__(self, train_list, train_path, num_frames, **kwargs):
         # Load data & labels
+        self.num_frames = num_frames
         self.data_list = []
         self.data_label = []
         with open(train_path + '/' + train_list, 'r', encoding='utf-8') as file:
@@ -10,11 +18,21 @@ class train_loader(object):
                 self.data_list.append(fileName)
                 self.data_label.append(label)
 
-    def __getitem__(self, item):
-        return
+    def __getitem__(self, index):
+        # Read the wavfile and randomly select the segment
+        audio, sr = soundfile.read(self.data_list[index])
+        # compute the wav length
+        length = self.num_frames * 160 + 240
+        # If the audio length is shorter than the required wav length, we just need to join the wav.
+        if audio.shape[0] <= length:
+            shortage = length - audio.shape[0]
+            audio = numpy.pad(audio, (0, shortage), 'wrap')
+        # Extract a fixed length of data from an audio randomly.
+        start_frame = numpy.int64(random.random() * (audio.shape[0] - length))
+        audio = audio[start_frame:start_frame + length]
+        audio = numpy.stack([audio], axis=0)
+        # return data and lable
+        return torch.FloatTensor(audio[0]), self.data_label[index]
 
     def __len__(self):
         return len(self.data_list)
-
-    def getList(self):
-        return self.data_list
--- main.py
+++ main.py
@@ -1,63 +1,13 @@
-# 这是一个示例 Python 脚本。
-import librosa
-import numpy
-import torch
-import torchaudio
-from matplotlib import pyplot as plt
-
 from dataLoader import train_loader
-from model import ECAPA_TDNN
 
-n_fft = 1024
-win_length = None
-hop_length = 512
-n_mels = 128
+train_Loader = train_loader('train_list.txt', './data', 2)
 
 
-def getTorchMel(x, sample_rate):
-    # mel_spectrogram = torchaudio.transforms.MelSpectrogram(sample_rate, n_fft=512, win_length=400, hop_length=160,
-    #                                                        f_min=20, f_max=7600, window_fn=torch.hamming_window,
-    #                                                        n_mels=80)
-    mel_spectrogram = torchaudio.transforms.MelSpectrogram(
-        sample_rate=16000,
-        n_fft=n_fft,
-        win_length=win_length,
-        hop_length=hop_length,
-        center=True,
-        pad_mode="reflect",
-        power=2.0,
-        norm="slaney",
-        n_mels=n_mels,
-        mel_scale="htk",
-    )
-    return mel_spectrogram(x)
+def execute_my_test_func():
+    for i in range(20):
+        audio, label = train_Loader.__getitem__(i)
+        print(f'audio.shape:{audio.shape},label: {label}')
 
-
-def plot_spectrogram(specgram, title=None, ylabel="freq_bin", ax=None):
-    print(librosa.power_to_db(specgram))
-    if ax is None:
-        _, ax = plt.subplots(1, 1)
-    if title is not None:
-        ax.set_title(title)
-    ax.set_ylabel(ylabel)
-    ax.imshow(librosa.power_to_db(specgram), origin="lower", aspect="auto", interpolation="nearest")
-    plt.show()
-
-
-net = ECAPA_TDNN(1024)
-
-print(train_loader('train_list.txt', './data').getList())
 
 if __name__ == '__main__':
-    warAudio, sr = librosa.load('./7061-6-0-0.wav')
-    # melspec = getTorchMel(torch.from_numpy(warAudio), sr)
-    # print(melspec.shape)
-    # plot_spectrogram(melspec, title="MelSpectrogram - torchaudio", ylabel="mel freq")
-    # warAudio, sr = librosa.load('./20231114153557.wav')
-    # melspec = getTorchMel(torch.from_numpy(warAudio), sr)
-    # print(melspec.shape)
-    # plot_spectrogram(melspec, title="MelSpectrogram - torchaudio", ylabel="mel freq")
-    # print(torch.from_numpy(warAudio).shape)
-    # net.forward(torch.from_numpy(warAudio), aug=False)
-    # warAudio = numpy.stack(warAudio, axis=0)
-    # print(torch.tensor(warAudio))
+    execute_my_test_func()
