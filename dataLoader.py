d9be98ce49b3320b25bcd33c2ad3a641755e30ee
asiter <1139933860@qq.com>
2023-12-14 17:13:44 +0800

    feat:Modify the data reading mode to convert multi-channel audio to a single channel

--- dataLoader.py
+++ dataLoader.py
@@ -19,10 +19,14 @@ class train_loader(object):
                 self.data_label.append(label)
 
     def __getitem__(self, index):
+        # In the later stage, noise will be added to increase the accuracy of training.
         # Read the wavfile and randomly select the segment
         audio, sr = soundfile.read(self.data_list[index])
+        # Testing Mono Audio
+        # audio, sr = soundfile.read("20231114153557.wav")
         # compute the wav length
-        length = self.num_frames * 160 + 240
+        # + 240
+        length = self.num_frames * 160
         # If the audio length is shorter than the required wav length, we just need to join the wav.
         if audio.shape[0] <= length:
             shortage = length - audio.shape[0]
@@ -31,8 +35,10 @@ class train_loader(object):
         start_frame = numpy.int64(random.random() * (audio.shape[0] - length))
         audio = audio[start_frame:start_frame + length]
         audio = numpy.stack([audio], axis=0)
-        # return data and lable
-        return torch.FloatTensor(audio[0]), self.data_label[index]
+        # Process dual-channel audio into single-channel audio.
+        if len(audio.shape) >= 3:
+            audio = audio[:, :, 0]
+        return torch.FloatTensor(audio[0]), int(self.data_label[index])
 
     def __len__(self):
         return len(self.data_list)
