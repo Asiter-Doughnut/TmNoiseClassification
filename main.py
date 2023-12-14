ac54df7d4be019ca68288f5771955379f8ce6974
asiter <1139933860@qq.com>
2023-12-14 17:24:48 +0800

    feat:test ecapamodel and dataLoader

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import soundfile
+import torch
+import torchaudio
+
+import torch.nn as nn
+
 from dataLoader import train_loader
 
-train_Loader = train_loader('train_list.txt', './data', 2)
+from EcapaModel import EcapaModel
+
+# audio, sr = soundfile.read('20231114153557.wav')
 
 
-def execute_my_test_func():
-    for i in range(20):
-        audio, label = train_Loader.__getitem__(i)
-        print(f'audio.shape:{audio.shape},label: {label}')
+# test_list
+train_Loader = train_loader('test_list.txt', './data', 300)
+trainLoader = torch.utils.data.DataLoader(train_Loader, batch_size=64, shuffle=True, num_workers=0,
+                                          drop_last=True)
 
+epoch = 1
+# lr, lr_decay, C, n_class, m, s, test_step
+s = EcapaModel(lr=0.001, lr_decay=0.97, C=512, m=0.2, s=30, n_class=10, test_step=1)
+#
 
 if __name__ == '__main__':
-    execute_my_test_func()
+    while (1):
+        ## Training for one epoch
+        loss, lr, acc = s.train_network(epoch=epoch, loader=trainLoader)
+        if epoch >= 80:
+            quit()
+        epoch += 1
